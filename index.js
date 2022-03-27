d2cbbd620bb89f9cf0f1640ac5462669a4a4fdce
v6b <99478294+v6b@users.noreply.github.com>
2022-03-27 15:40:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const readme = `\
 ---
 ![](https://github-readme-stats.vercel.app/api?username=v6b&bg_color=30,e96443,904e95&title_color=fff&text_color=fff&layout=compact)
 ![](https://github-readme-stats.vercel.app/api/top-langs/?username=v6b&layout=compact&bg_color=30,e96443,904e95&title_color=fff&text_color=fff)  
-![](https://gcore.jsdelivr.net/gh/v6b/v6b/main/assets/github-contribution-grid-snake.svg)
+![](https://gcore.jsdelivr.net/gh/v6b/v6b@main/assets/github-contribution-grid-snake.svg)
 \
 `
 
